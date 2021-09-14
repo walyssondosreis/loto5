6bc2bc961672e664fe3033c3a8b4cbc1504e2c4f
Walysson dos Reis <38730743+walyssondosreis@users.noreply.github.com>
2021-09-13 22:36:54 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+# JOGO/NUMEROS
+
+class Jogo:
+    '''Classe de jogos/números.'''
+    def __init__(self, jogo):
+        self.jogo = jogo
+        print("JOGO: ",self.jogo)
+
+    def seq_verificar(self):
+        '''Metodo que retorna lista com sequência das divisões em grupo.'''
+        p1, p2, p3, p4, p5 = 0, 0, 0, 0, 0
+        for x in self.jogo:
+            if x < 6: p1 += 1
+            if 5 < x < 11: p2 += 1
+            if 10 < x < 16: p3 += 1
+            if 15 < x < 21: p4 += 1
+            if x > 20: p5 += 1
+        seq = [p1, p2, p3, p4, p5]
+        return seq
+
+    def pontos_verificar(self, res):
+        '''Metodo que retorna dict. com lista de erros, lista de acertos e número de pontos.'''
+        erros = list(set(self.jogo) - set(res))
+        acertos=list(set(self.jogo) - set(erros))
+        pontos=len(acertos)
+        return {'erros':erros,'acertos':acertos,'pontos':pontos}
+    def duplicado_verificar(self,comp):
+        dup=list(set(self.jogo)-set(comp))
+        if not dup: return True
+        else: return False
+
+
---
+++ main.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# LOTO5
+from jogo import *
+
+
+jogo1 = [1, 4, 5, 9, 10, 15, 13, 16, 18, 19, 20, 21, 23, 24, 25]
+jogo2 = [1, 3, 4, 7, 9, 10, 11, 13, 14, 15, 17, 20, 21, 23, 25]
+jogo3 = [1, 3, 4, 7, 10, 12, 13, 14, 17, 18, 20, 22, 23, 24, 25]
+jogo4 = [1, 2, 3, 5, 7, 10, 11, 14, 15, 17, 18, 20, 22, 24, 25]
+jogo5 = [1, 3, 5, 7, 9, 10, 11, 12, 13, 14, 16, 17, 20, 23, 25]
+jogo6 = [2, 3, 4, 7, 9, 10, 11, 12, 14, 16, 20, 21, 22, 24, 25]
+
+resultado = [2, 1, 24, 6, 9, 15, 13, 12, 22, 3, 21, 23, 25, 17, 5]
+
+#GRUPO DE APOSTAS
+# id_aposta,seq,nome_aposta,numeros, pontos, concurso, apostador, grupo
+#GRUPO DE RESULTADOS
+
+
+print("RESULTADO: ",resultado)
+j1=Jogo(jogo2)
+print(j1.seq_verificar())
+print(j1.pontos_verificar(resultado))
+print(j1.duplicado_verificar(jogo2))
+
