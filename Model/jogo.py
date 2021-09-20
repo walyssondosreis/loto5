14cec622f354b9f018c8f2afeba3027ed42c51aa
walyssondosreis <walyssondosreis@gmail.com>
2021-09-20 18:00:17 -0300

    Alter1

--- Model/jogo.py
+++ Model/jogo.py
@@ -9,20 +9,20 @@ class Jogo(NumList):
     def __init__(self,num_list):
         super().__init__(num_list=num_list)
 
-    def calc_pts(self, res):
+    def pts(self, res):
         '''Metodo que retorna dict. com lista de erros, lista de acertos e número de pontos.'''
         erros = list(set(self.nums) - set(res.num_list))
         acertos=list(set(self.nums) - set(erros))
         pts=len(acertos)
         return {'erros':erros,'acertos':acertos,'pts':pts}
 
-    def verificar_dup(self,comp):
+    def dup(self,comp):
         '''Método que retorna boleano para "jogo duplicado?"'''
         dup=list(set(self.jogo)-set(comp))
         if not dup: return True
         else: return False
     
-    def comp_jogos(self):
+    def comp(self):
         '''Método que compara jogo com resultado'''
         pass
 
