792776f4df7e216e7e22f4fce0558c20d631225c
Walysson dos Reis <walyssondosreis@email.com>
2022-07-02 14:13:40 -0300

    andamento form

--- src/visao/cadastroAposta.php
+++ src/visao/cadastroAposta.php
@@ -10,55 +10,95 @@ require_once "header.php";
     <main>
         <form action="" method="post">
 
-            <!-- Primeira sessão do formulário: Apresentação -->
-            <section class="container-fluid" style="background-color: yellow;">
-                <h2 class="text-center"> Bem vindo Walysson!</h2>
-                <p class="text-center"> Crie seus jogos facilmente e aposte já!</p>
-            </section>
+            <div class="container">
+                <!-- Apresentação -->
 
-            <!-- Segunda sessão do formulário: Botôes de preenchimento jogo -->
-            <section class="container-fluid">
-                <div class="container mb-3">
-                    CRIAR JOGO:
+                <div class="text-center">
+                    <h2> Bem vindo Walysson!</h2>
+                    <p> Crie seus jogos facilmente e aposte já!</p>
                 </div>
-                <div class="container mb-3">
-                    <!-- Grupo de botões -->
-                    <?php
-                    $idBotao = "btncheck";
-                    for ($i = 1; $i <= 25; $i++) :
-                    ?>
-                        <div class="btn-group" role="group">
-                            <input type="checkbox" class="btn-check " value=<?php echo $i ?> id=<?php echo $idBotao . $i ?> autocomplete="off">
-                            <label class="btn btn-outline-dark rounded-circle" for=<?php echo $idBotao . $i ?>> <?php echo str_pad($i, 2, '0', STR_PAD_LEFT) ?></label>
-                        </div> <!-- Fim grupo de botões -->
-                    <?php endfor ?>
-                </div> <!-- Fim row -->
 
-                <div class="container"><a href="">SELECIONAR JOGO SALVO</a></div>
-            </section>
 
-            <!-- Terceira sessão do formulário: Grupo de aposta -->
-            <section>
+                <!-- Botôes de preenchimento jogo -->
                 <div>
+                    <div class="container mb-3 text-center">
+                        Escolha de 15 a 18 números
+                    </div>
+                    <div class="container mb-3">
+                        <!-- Grupo de botões -->
+                        <?php
+                        $idBotao = "btncheck";
+                        for ($i = 1; $i <= 25; $i++) :
+                        ?>
+                            <div class="btn-group" role="group">
+                                <input type="checkbox" class="btn-check " value=<?php echo $i ?> id=<?php echo $idBotao . $i ?> autocomplete="off">
+                                <label class="btn btn-outline-dark rounded-circle" for=<?php echo $idBotao . $i ?>> <?php echo str_pad($i, 2, '0', STR_PAD_LEFT) ?></label>
+                            </div> <!-- Fim grupo de botões -->
+                        <?php endfor ?>
+                    </div> <!-- Fim row -->
+                    <div class="container mb-3 text-center">
+                        <a href="">SELECIONAR JOGO SALVO</a>
+                    </div>
+                </div>
+
+                <!-- Botões de finalização/submissão -->
+
+                <div class="container text-center">
+                    <button type="button" class="btn btn-primary btn-sm col">Limpar aposta</button>
+                    <button type="button" class="btn btn-primary btn-sm col">Adicionar carrinho</button>
+                </div>
+
+
+            </div>
+            <!-- Tabela carrinho de compras -->
+            <div class="container">
+                <h3>Carrinho de Apostas</h3>
+                <!--Grupo de aposta -->
+                <div class="container text-center mb-3">
                     <!-- Grupo de aposta irá informar o grupo e um botão para alterar -->
-                    <p>Grupo da Aposta: Lotofacil </p>
-                    <button type="button" class="btn btn-primary btn-sm">Alterar</button>
+                    Grupo da Aposta: Lotofacil
+                    <a href="">(Alterar)</a>
                 </div>
-            </section>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!-- Botões de finalização/submissão -->
 
-            <!-- Quarta sessão do formulário: Botões de finalização/submissão -->
-            <section>
-                <div>
-                    <button type="button" class="btn btn-primary btn-sm">Adicionar carrinho</button>
-                    <button type="button" class="btn btn-primary btn-sm">Finalizar aposta</button>
-                    <button type="button" class="btn btn-primary btn-sm">Limpar aposta</button>
-                    <button type="button" class="btn btn-primary btn-sm">Limpar carrinho</button>
+                <div class="container text-center">
+                    <button type="button" class="btn btn-primary btn-sm col">Limpar carrinho</button>
+                    <button type="button" class="btn btn-primary btn-sm col ">Finalizar aposta</button>
                 </div>
-            </section>
-
-
+            </div>
         </form> <!-- Fecha formulário-->
+
     </main>
+
     <?php
     require_once "footer.php";
     ?>
--- src/visao/navbar.php
+++ src/visao/navbar.php
@@ -1,6 +1,6 @@
 
-<nav class="navbar bg-light">
-  <div class="container-fluid">
+<nav class="navbar bg-light" >
+  <div class="container">
     <a class="navbar-brand" href="#">
       <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/MetroDF_L%C3%ADnea_5.svg/480px-MetroDF_L%C3%ADnea_5.svg.png" alt="" width="30" height="30" class="d-inline-block align-text-top"> -->
       <img src="https://images.squarespace-cdn.com/content/v1/5e880238c85a337142d4224b/1585971803315-8RYACQ7Q34DK1L0W7AMC/5.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
