f4fa03e0f19a9aa9e620b4b1fec418b187ba9fff
Walysson dos Reis <walyssondosreis@email.com>
2022-07-01 23:29:12 -0300

    front da tela de cadastro aposta

--- src/visao/cadastroAposta.php
+++ src/visao/cadastroAposta.php
@@ -6,15 +6,22 @@ require_once "header.php";
     <?php
     require_once "navbar.php";
     ?>
-    <div class="container-fluid">
-        <form action="" method="post">
-            <div class="row">
-                <p> Bem vindo Walysson!</p>
-            </div>
 
-            <div class="row">
-                <div class="col">
-                    <p>Criar novo jogo:</p>
+    <main>
+        <form action="" method="post">
+
+            <!-- Primeira sessão do formulário: Apresentação -->
+            <section class="container-fluid" style="background-color: yellow;">
+                <h2 class="text-center"> Bem vindo Walysson!</h2>
+                <p class="text-center"> Crie seus jogos facilmente e aposte já!</p>
+            </section>
+
+            <!-- Segunda sessão do formulário: Botôes de preenchimento jogo -->
+            <section class="container-fluid">
+                <div class="container mb-3">
+                    CRIAR JOGO:
+                </div>
+                <div class="container mb-3">
                     <!-- Grupo de botões -->
                     <?php
                     $idBotao = "btncheck";
@@ -25,37 +32,33 @@ require_once "header.php";
                             <label class="btn btn-outline-dark rounded-circle" for=<?php echo $idBotao . $i ?>> <?php echo str_pad($i, 2, '0', STR_PAD_LEFT) ?></label>
                         </div> <!-- Fim grupo de botões -->
                     <?php endfor ?>
-                </div> <!-- Fim col -->
-                <div class="col">
-                    <p> Selecionar dos meus jogos:</p>
-                </div> <!-- Fim col -->
-            </div> <!-- Fim row -->
+                </div> <!-- Fim row -->
 
-            <div class="row">
-                <p> Selecionar grupo da aposta</p>
-                <div class="col">
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected value="1">Lotofacil</option>
-                        <option value="2">Vox</option>
-                        <option value="3">Grupo 2</option>
-                        <option value="4">Novo Grupo</option>
-                    </select>
+                <div class="container"><a href="">SELECIONAR JOGO SALVO</a></div>
+            </section>
+
+            <!-- Terceira sessão do formulário: Grupo de aposta -->
+            <section>
+                <div>
+                    <!-- Grupo de aposta irá informar o grupo e um botão para alterar -->
+                    <p>Grupo da Aposta: Lotofacil </p>
+                    <button type="button" class="btn btn-primary btn-sm">Alterar</button>
                 </div>
-                <div class="col input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+            </section>
+
+            <!-- Quarta sessão do formulário: Botões de finalização/submissão -->
+            <section>
+                <div>
+                    <button type="button" class="btn btn-primary btn-sm">Adicionar carrinho</button>
+                    <button type="button" class="btn btn-primary btn-sm">Finalizar aposta</button>
+                    <button type="button" class="btn btn-primary btn-sm">Limpar aposta</button>
+                    <button type="button" class="btn btn-primary btn-sm">Limpar carrinho</button>
                 </div>
-            </div>
-
-            <div class="row">
-                <p> botao de adicionar carrinho</p>
-                <p> botao de finalizar aposta </p>
-                <p> botao de limpar aposta </p>
-            </div>
+            </section>
 
 
-        </form>
-    </div> <!-- Fim continer-fluid -->
+        </form> <!-- Fecha formulário-->
+    </main>
     <?php
     require_once "footer.php";
     ?>
